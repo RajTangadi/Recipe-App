7907f38e280877d071c2bb61d658d39d3fc017c2
Raj <rajtangadi65@gmail.com>
2023-03-12 21:51:18 +0530

    build

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import { userRouter } from "./routes/users.js";
 import { UserModel } from "./model/Users.js";
 import { recipesRouter } from "./routes/recipes.js";
+import path from "path";
 
 dotenv.config();
 const port = process.env.PORT || 3001;
@@ -16,7 +17,17 @@ app.use(cors());
 app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
 
-app.listen(port, () => console.log(`server listening on ${port}`));
+// serving the frontend
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 
 // mongo connection
 
@@ -25,3 +36,5 @@ const connectDb = async () => {
 };
 
 connectDb();
+
+app.listen(port, () => console.log(`server listening on ${port}`));
